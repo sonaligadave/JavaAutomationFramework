353bcbd87c78960bbd4e08030a87e1d95db9e5b9
sp <gadave.sonali22@gmail.com>
2021-03-02 21:43:17 -0800

    graphQL api Test

--- src/test/java/APITest/GraphQLApiTest.java
+++ src/test/java/APITest/GraphQLApiTest.java
@@ -1,13 +1,15 @@
 package APITest;
 
+import Actions.GraphQlActions;
 import org.testng.annotations.Test;
 
 
-public class BaseApiTest {
+public class GraphQLApiTest {
 
     @Test
     public void postCallTest() {
-
+        GraphQlActions graphQlActions = new GraphQlActions();
+        graphQlActions.postUser();
     }
 
     @Test
